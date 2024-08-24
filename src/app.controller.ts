2d2ad4af193ed5e03613396149adb8020601620d
ishaan766 <ishaan766@gmail.com>
2024-08-24 13:00:43 +0530

    added mysql,redis and more integrations..

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, UseFilters } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,7 +6,32 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
+  getHello(): Promise<string> {
     return this.appService.getHello();
+  }
+
+  @Get('param/:param')
+  getParam(@Param('param') param: string): Promise<string> {
+    return this.appService.getParam(param);
+  }
+
+  @Get('exception')
+  throwError(): Promise<void> {
+    return this.appService.throwError();
+  }
+
+  @Get('/api')
+  async handleApiCall(): Promise<string> {
+    return this.appService.callApi();
+  }
+
+  @Get('mysql')
+  queryMySQL(): Promise<string> {
+    return this.appService.queryMySQL();
+  }
+
+  @Get('redis')
+  callRedis(): Promise<string> {
+    return this.appService.callRedis();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,73 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Res } from '@nestjs/common';
+import { createConnection } from 'mysql2/promise';
+import { trace } from '@opentelemetry/api';
+import axios from 'axios';
+
+const redis = require("redis");
+
+
+const redisClient = redis.createClient({
+  url: "redis://redis:6379",
+});
+redisClient.connect().catch((err) => {
+  if (err) {
+    if (process.env.CUBE_DOCKER_COMPOSE) throw err;
+  } else {
+    console.log("redis connected!");
+  }
+});
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  private readonly mysqlClient;
+  cacheManager: any;
+
+  constructor() {
+    this.mysqlClient = createConnection({
+      host: 'mysql',
+      user: 'root',
+      password: 'root',
+      database: 'test',
+    });
+
+    this.mysqlClient.then(() => console.log('MySQL connected!')).catch((err) => {
+      if (process.env.CUBE_DOCKER_COMPOSE) throw err;
+    });
+  }
+
+  async getHello(): Promise<string> {
+    return 'Hello';
+  }
+
+  async getParam(param: string): Promise<string> {
+    return `Got param ${param}`;
+  }
+
+  async throwError(): Promise<void> {
+    throw new Error('Sample exception');
+  }
+
+  async callApi(): Promise<string> {
+    await axios.get('http://localhost:3000/');
+    return 'API called';
+  }
+
+  async queryMySQL(): Promise<string> {
+    const [rows] = await (await this.mysqlClient).execute('SELECT NOW()');
+    return rows[0]['NOW()'];
+  }
+
+  async callRedis(): Promise<string> {
+    await redisClient.set('foo', 'bar');
+    return ('Redis called');
+  }
+
+  errorHandler(err ,req, res, next) {
+    const span = trace.getActiveSpan();
+    if (span) {
+      span.recordException(err);
+    }
+    next (err); 
   }
 }
+
