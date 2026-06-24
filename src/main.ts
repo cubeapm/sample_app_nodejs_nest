53b0c42e9a79a05d57473ba5e4c6a9f1db3d3d04
anim3-sh <animesh@cubeapm.com>
2026-06-24 14:30:10 +0530

    add code for manual sdk instrument and dd-tracer

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,20 @@
+// Code level SDK implementation for dd-trace
+// import tracer from 'dd-trace';
+// tracer.init({
+//   service: "<app_name>",
+
+//   // Send data to CubeAPM.
+//   url: "http://host.docker.internal:3130",
+
+//   // optional settings
+//   // env: "myenv",
+//   // version: "1.2.3",
+//   // tags: {
+//   //   mykey1: "myvalue1",
+//   //   mykey2: "myvalue2"
+//   // },
+// });
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
